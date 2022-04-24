9948235ba3c0e19ca5396b4a464afbc73f0828f7
Apurvakulkarni06 <apurvakulkarni098@gmail.com>
2022-04-24 15:00:50 +0530

    initial commit

---
+++ stopwatch/app.js
@@ -0,0 +1,5 @@
+var subHeading = "Stopwatch App using Typescript";
+window.onload = function () {
+    var subHeadingTag = document.getElementsByClassName("sub-heading")[0];
+    subHeadingTag.textContent = subHeading;
+};
---
+++ stopwatch/app.ts
@@ -0,0 +1,9 @@
+const subHeading : string = "Stopwatch App using Typescript";
+
+window.onload = () =>{
+    const subHeadingTag = document.getElementsByClassName("sub-heading")[0]
+
+    subHeadingTag.textContent = subHeading
+}
+
+
