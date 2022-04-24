f4f18937e34635634fa711aa10c8486e4acb365c
Apurvakulkarni06 <apurvakulkarni098@gmail.com>
2022-04-24 18:15:42 +0530

    feat: adding logic to update timings on actions

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -1,5 +1,64 @@
-var subHeading = "Stopwatch App using Typescript";
-window.onload = function () {
-    var subHeadingTag = document.getElementsByClassName("sub-heading")[0];
-    subHeadingTag.textContent = subHeading;
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
 };
+var startButton = document.getElementById("start");
+var stopButton = document.getElementById("stop");
+var resetButton = document.getElementById("reset");
+var hourText = document.getElementById("hour");
+var minuteText = document.getElementById("minute");
+var secondText = document.getElementById("second");
+var InitialClockTiming = {
+    hour: 0,
+    minute: 0,
+    second: 0
+};
+var clockTiming = {
+    hour: 0,
+    minute: 0,
+    second: 0
+};
+var timerId = null;
+startButton.addEventListener("click", function () {
+    console.log("start the watch");
+    timerId = setInterval(setClock, 1000);
+});
+stopButton.addEventListener("click", function () {
+    console.log("stop the watch");
+    clearInterval(timerId);
+    clockTiming = __assign({}, InitialClockTiming);
+    setClockContent(clockTiming);
+});
+resetButton.addEventListener("click", function () {
+    console.log("reset the watch");
+    clockTiming = __assign({}, InitialClockTiming);
+    setClockContent(clockTiming);
+});
+function setClock() {
+    var existingSeconds = clockTiming.second;
+    var existingMinutes = clockTiming.minute;
+    clockTiming.second += 1;
+    if (existingSeconds > 5) {
+        // add in minutes
+        console.log("updating the minute");
+        clockTiming.second = 0;
+        clockTiming.minute += 1;
+    }
+    if (existingMinutes > 5) {
+        // add in minutes
+        console.log("updating the hour");
+        clockTiming.second = 0;
+        clockTiming.minute = 0;
+        clockTiming.hour += 1;
+    }
+    setClockContent(clockTiming);
+}
+function setClockContent(timersData) {
+    secondText.textContent = timersData.second.toLocaleString();
+    minuteText.textContent = timersData.minute.toLocaleString();
+    hourText.textContent = timersData.hour.toLocaleString();
+}
--- stopwatch/app.ts
+++ stopwatch/app.ts
@@ -1,9 +1,76 @@
-const subHeading : string = "Stopwatch App using Typescript";
+const startButton : HTMLElement = document.getElementById("start");
+const stopButton : HTMLElement = document.getElementById("stop");
+const resetButton : HTMLElement = document.getElementById("reset");
 
-window.onload = () =>{
-    const subHeadingTag = document.getElementsByClassName("sub-heading")[0]
+const hourText : HTMLElement = document.getElementById("hour");
+const minuteText : HTMLElement = document.getElementById("minute");
+const secondText : HTMLElement = document.getElementById("second");
 
-    subHeadingTag.textContent = subHeading
+type clockTimingType = {
+    hour : number;
+    minute: number;
+    second: number;
 }
+let InitialClockTiming : clockTimingType = {
+    hour : 0,
+    minute : 0,
+    second  : 0
+}
+let clockTiming : clockTimingType = {
+    hour : 0,
+    minute : 0,
+    second  : 0
+}
+let timerId = null;
+
+startButton.addEventListener("click", ()=>{
+    console.log("start the watch")
+    timerId = setInterval(setClock, 1000)
+
+})
+stopButton.addEventListener("click", ()=>{
+    console.log("stop the watch");
+    clearInterval(timerId);
+    clockTiming = {...InitialClockTiming};
+    setClockContent(clockTiming)
+})
+resetButton.addEventListener("click", ()=>{
+    console.log("reset the watch");
+    clockTiming = {...InitialClockTiming}
+    setClockContent(clockTiming)
+})
+
+function setClock() {
+
+    let existingSeconds = clockTiming.second;
+    let existingMinutes = clockTiming.minute; 
+    
+    clockTiming.second += 1;
+    
+    if(existingSeconds >= 5){
+        // add in minutes
+        console.log("updating the minute")
+        clockTiming.second = 0;
+        clockTiming.minute += 1;
+        
+    }
+
+    if(existingMinutes >= 5){
+         // add in minutes
+         console.log("updating the hour")
+         clockTiming.second = 0;
+         clockTiming.minute = 0;
+         clockTiming.hour += 1;
+         
+    }
+    setClockContent(clockTiming)
+}
+
+function setClockContent(timersData: clockTimingType){
+    secondText.textContent = timersData.second.toLocaleString()
+    minuteText.textContent = timersData.minute.toLocaleString()
+    hourText.textContent = timersData.hour.toLocaleString()
+}
+
 
 
